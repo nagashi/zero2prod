9f4ebea80eb0204dc2cd3360b95fd2696ab19efb
Charles ORiley <ceoriley@gmail.com>
2022-04-18 15:45:47 -0500

    3.5.1.1 Clean Up: 47

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use zero2prod::run;
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
