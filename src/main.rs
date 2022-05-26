9c2797da35884c162b25e85db8cba05d7004c0c2
Charles ORiley <ceoriley@gmail.com>
2022-05-26 07:38:30 -0500

    3.8.5.2.2 Reading A Configuration File

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
