3ef258faeee9d1f5629ebee85074ccbd51f21e33
Charles ORiley <ceoriley@gmail.com>
2022-06-13 23:47:33 -0500

    Completed: 4.5.7 tracing-bunyan-formatter

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,13 +13,18 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     // Let's generate a random unique identifier
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
     );
-    tracing::info!("Saving new subscriber details in the database");
+
+    let _request_span_guard = request_span.enter();
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
         r#"
@@ -31,14 +37,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.as_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("request_id {request_id} - New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            log::error!("request_id {request_id} - Failed to execute query: {:?}", e);
+            // Yes, this error log falls outside of `query_span`
+            // We'll rectify it later, pinky swear!
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -119,5 +119,3 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
-
-
