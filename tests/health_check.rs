f24ae85cae87f34ca6bc263465609f3602c3cc59
Charles ORiley <ceoriley@gmail.com>
2022-09-22 23:46:37 -0500

    3.8.5.5 Updating Our CI Pipeline

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
 use zero2prod::startup::run;
 
@@ -33,6 +35,13 @@ fn spawn_app() -> String {
 async fn subscribe_return_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     // Act
@@ -47,6 +56,14 @@ async fn subscribe_return_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
