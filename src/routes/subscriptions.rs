afdb64509b022d4ea1eeb8dac5d1ccc1e30815c0
Charles ORiley <ceoriley@gmail.com>
2022-10-15 00:17:07 -0500

    4.5.8 tracing-log

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,6 +11,21 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    /*
+    Spans, like logs, have an associated level
+    `info_span` creates a span at the info-level
+    */
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "request_id {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -20,12 +36,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.as_ref())
+    .execute(pool.get_ref())
+    /*
+    First we attach the instrumentation, then we `.await` it
+    */
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {request_id} - New subscriber details have been saved.");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("request_id {request_id} - Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
