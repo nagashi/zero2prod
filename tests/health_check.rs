9fbb1359805043240c154adbe044744dd33371e0
Charles ORiley <ceoriley@gmail.com>
2022-10-15 03:50:32 -0500

    4.5.11 Logs For Integration Tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,6 +3,29 @@ use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
+
+/*
+Ensure that the `tracing` stack is only initialised one using `once_cell 
+*/
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    /*
+    We cannot assign the output of `get_subscriber` to a variable based on the value
+    of `TEST_LOG` because the sink is part of the type returned by `get_subscriber`,
+    therefore they are not the same type. We could work around it, but this is the
+    most straight-forward way of moving forward.
+    */
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };    
+});
 
 pub struct TestApp {
     pub address: String,
@@ -10,10 +33,18 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    /*
+    The first time `initialize` is invoked the code in `TRACING` is executed.
+    All other invocations will instead skip execution.
+    */
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
+    /*
+    We retrieve the port assigned to us by the OS
+    */
     let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
+    let address = format!("http://127.0.0.1:{port}");
 
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
@@ -28,7 +59,9 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    // Create database
+    /*
+    Create database
+    */
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
         .expect("Failed to connect to Postgres");
@@ -37,7 +70,9 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
 
-    // Migrate database
+    /*
+    Migrate database
+    */
     let connection_pool = PgPool::connect(&config.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
@@ -57,7 +92,9 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        // Use the returned application address
+        /*
+        Use the returned application address
+        */
         .get(&format!("{}/health_check", &app.address))
         .send()
         .await
@@ -121,7 +158,9 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         assert_eq!(
             400,
             response.status().as_u16(),
-            // Additional customised error message on test failure
+            /*
+            Additional customised error message on test failure
+            */
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
