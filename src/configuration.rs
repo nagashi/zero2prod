535f6c6f0dcb582d5a8e0ba70fd2b16a329c45fb
Charles ORiley <ceoriley@gmail.com>
2022-10-21 22:14:50 -0500

    5.3.7 Database Connectivity

--- src/configuration.rs
+++ src/configuration.rs
@@ -48,8 +48,10 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
     
-    // Detect the running environment.
-    // Default to `local` if unspecified.
+    /*
+    Detect the running environment.
+    Default to `local` if unspecified.
+    */
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
